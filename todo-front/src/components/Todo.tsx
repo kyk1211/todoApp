2f68271a8175994ccdbe75865ab86108212e7175
곽윤구 <dbsrndi123@naver.com>
2022-05-30 22:54:52 +0900

    refactor directory

---
+++ todo-front/src/components/Todo.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Todo() {
+  return (
+    <div className="Todo">
+      <input type={"checkbox"} id="todo0" name="todo0" value={"todo0"} />
+      <label htmlFor="todo0">Make Todo component</label>
+    </div>
+  );
+}
